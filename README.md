c851ac1ba18d4b4ca7a1be8c00d35345f0c0e5d1
Ana Carolina <aanaakaarolina@gmail.com>
2026-06-16 17:41:14 -0300

    avanço do projeto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # trabalho-pratico-1
 Trabalho Prático 1 - DIW
+
+O participe + é um projeto que ajuda projetos sociais. Com ele, pessoas de fora ficam sabendo sobre o projeto e podem ajudar as instituições de várias formas.
---
+++ assets/script.js
@@ -0,0 +1,402 @@
+const dados = {
+    "projetos": [
+        {
+            "id": 1,
+            "nome": "Casa da Comunidade",
+            "descricao": "Apoio comuntário a jovens e adultos na região metropolitana de Belo Horizonte",
+            "local": "Belo Horizonte",
+            "pessoas_atendidas": 681,
+            "voluntarios": 56,
+            "imagens": [
+                {
+                    "id": 1,
+                    "link": "https://images.unsplash.com/photo-1695938746747-ec185ea81325?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                },
+                {
+                    "id": 2,
+                    "link": "https://images.unsplash.com/photo-1526976668912-1a811878dd37?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                },
+                {
+                    "id": 3,
+                    "link": "https://images.unsplash.com/photo-1593113598332-cd288d649433?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                },
+                {
+                    "id": 4,
+                    "link": "https://images.unsplash.com/photo-1618521715147-29e4b97e2ebd?q=80&w=704&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                }
+            ],
+            "redes_sociais": [
+                {
+                    "id": 1,
+                    "nome": "WhatsApp",
+                    "link": "wa.me/5531000000"
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "nome": "Jogadores em Ação",
+            "descricao": "Projeto que tira crianças da rua e as tornam jogadores de futebol.",
+            "local": "Belo Horizonte",
+            "pessoas_atendidas": 136,
+            "voluntarios": 15,
+            "imagens": [
+                {
+                    "id": 1,
+                    "link": "https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?q=80&w=838&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                },
+                {
+                    "id": 2,
+                    "link": "https://images.unsplash.com/photo-1580560230671-61e01dfdb285?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y3JpYW4lQzMlQTdhJTIwZnV0ZWJvbHxlbnwwfHwwfHx8Mg%3D%3D"
+                },
+                {
+                    "id": 3,
+                    "link": "https://images.unsplash.com/photo-1604651684549-962bd68d7c5c?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y3JpYW4lQzMlQTdhJTIwZnV0ZWJvbHxlbnwwfHwwfHx8Mg%3D%3D"
+                },
+                {
+                    "id": 4,
+                    "link": "https://images.unsplash.com/photo-1611812695436-b0da42923bd6?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNyaWFuJUMzJUE3YSUyMGZ1dGVib2x8ZW58MHx8MHx8fDI%3D"
+                }
+            ],
+            "redes_sociais": [
+                {
+                    "id": 1,
+                    "nome": "WhatsApp",
+                    "link": "wa.me/5531555555"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "nome": "Mães Amigas",
+            "descricao": "Encontro de mães para troca de experiências.",
+            "local": "Belo Horizonte",
+            "pessoas_atendidas": 1987,
+            "voluntarios": 462,
+            "imagens": [
+                {
+                    "id": 1,
+                    "link": "https://images.unsplash.com/photo-1758513359379-a1ccce73b09e?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                },
+                {
+                    "id": 2,
+                    "link": "https://images.unsplash.com/photo-1607748862156-7c548e7e98f4?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                },
+                {
+                    "id": 3,
+                    "link": "https://images.unsplash.com/photo-1531983412531-1f49a365ffed?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                },
+                {
+                    "id": 4,
+                    "link": "https://images.unsplash.com/photo-1526277015674-026cb84653ec?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                }
+            ],
+            "redes_sociais": [
+                {
+                    "id": 1,
+                    "nome": "WhatsApp",
+                    "link": "wa.me/5531555555"
+                }
+            ]
+        },
+        {
+            "id": 4,
+            "nome": "Biblioteca para Todos",
+            "descricao": "Projeto de incentivo à leitura com empréstimo gratuito de livros para crianças e adolescentes.",
+            "local": "Betim",
+            "pessoas_atendidas": 61,
+            "voluntarios": 7,
+            "imagens": [
+                {
+                    "id": 1,
+                    "link": "https://images.unsplash.com/photo-1507842217343-583bb7270b66"
+                },
+                {
+                    "id": 2,
+                    "link": "https://images.unsplash.com/photo-1525715843408-5c6ec44503b1?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGxlaXR1cmF8ZW58MHx8MHx8fDI%3D"
+                },
+                {
+                    "id": 3,
+                    "link": "https://images.unsplash.com/photo-1521714161819-15534968fc5f?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGxlaXR1cmF8ZW58MHx8MHx8fDI%3D"
+                },
+                {
+                    "id": 4,
+                    "link": "https://images.unsplash.com/photo-1573142143200-2a6d95ae7352?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGxlaXR1cmF8ZW58MHx8MHx8fDI%3D"
+                }
+            ],
+            "redes_sociais": [
+                {
+                    "id": 1,
+                    "nome": "WhatsApp",
+                    "link": "wa.me/553133333333"
+                }
+            ]
+        },
+        {
+            "id": 5,
+            "nome": "Recicla BH",
+            "descricao": "Promove educação ambiental e coleta seletiva em comunidades da região metropolitana.",
+            "local": "Belo Horizonte",
+            "pessoas_atendidas": 84,
+            "voluntarios": 5,
+            "imagens": [
+                {
+                    "id": 1,
+                    "link": "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b"
+                },
+                {
+                    "id": 2,
+                    "link": "https://images.unsplash.com/photo-1610093674388-cee0337f2684?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmVjaWNsYWdlbXxlbnwwfHwwfHx8Mg%3D%3D"
+                },
+                {
+                    "id": 3,
+                    "link": "https://images.unsplash.com/photo-1591193686104-fddba4d0e4d8?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cmVjaWNsYWdlbXxlbnwwfHwwfHx8Mg%3D%3D"
+                },
+                {
+                    "id": 4,
+                    "link": "https://images.unsplash.com/photo-1561503412-852800622772?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cmVjaWNsYWdlbXxlbnwwfHwwfHx8Mg%3D%3D"
+                }
+            ],
+            "redes_sociais": [
+                {
+                    "id": 1,
+                    "nome": "WhatsApp",
+                    "link": "wa.me/553144444444"
+                }
+            ]
+        },
+        {
+            "id": 6,
+            "nome": "Música na Praça",
+            "local": "Ibirité",
+            "descricao": "Oficinas gratuitas de instrumentos musicais para jovens em situação de vulnerabilidade social.",
+            "pessoas_atendidas": 300,
+            "voluntarios": 19,
+            "imagens": [
+                {
+                    "id": 1,
+                    "link": "https://images.unsplash.com/photo-1511379938547-c1f69419868d"
+                },
+                {
+                    "id": 2,
+                    "link": "https://images.unsplash.com/photo-1583236753515-7e06aae56395?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bSVDMyVCQXNpY2ElMjBydWF8ZW58MHx8MHx8fDI%3D"
+                },
+                {
+                    "id": 3,
+                    "link": "https://images.unsplash.com/photo-1651694721718-7a72df522ae3?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bSVDMyVCQXNpY2ElMjBydWF8ZW58MHx8MHx8fDI%3D"
+                },
+                {
+                    "id": 4,
+                    "link": "https://images.unsplash.com/photo-1720455730407-e59f735d1a91?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bSVDMyVCQXNpY2ElMjBydWF8ZW58MHx8MHx8fDI%3D"
+                }
+            ],
+            "redes_sociais": [
+                {
+                    "id": 1,
+                    "nome": "WhatsApp",
+                    "link": "wa.me/553155555556"
+                }
+            ]
+        },
+        {
+            "id": 7,
+            "nome": "Horta Comunitária Verde Vida",
+            "descricao": "Cultivo coletivo de hortaliças para abastecer famílias em situação de vulnerabilidade.",
+            "local": "Belo Horizonte",
+            "pessoas_atendidas": 670,
+            "voluntarios": 20,
+            "imagens": [
+                {
+                    "id": 1,
+                    "link": "https://images.unsplash.com/photo-1466692476868-aef1dfb1e735"
+                },
+                {
+                    "id": 2,
+                    "link": "https://images.unsplash.com/photo-1563714193017-5a5fb60bc02b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aG9ydGF8ZW58MHx8MHx8fDI%3D"
+                },
+                {
+                    "id": 3,
+                    "link": "https://images.unsplash.com/photo-1515150144380-bca9f1650ed9?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvcnRhfGVufDB8fDB8fHwy"
+                },
+                {
+                    "id": 4,
+                    "link": "https://images.unsplash.com/photo-1611843467160-25afb8df1074?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvcnRhfGVufDB8fDB8fHwy"
+                }
+            ],
+            "redes_sociais": [
+                {
+                    "id": 1,
+                    "nome": "WhatsApp",
+                    "link": "wa.me/553188888888"
+                }
+            ]
+        },
+        {
+            "id": 8,
+            "nome": "Programadores do Futuro",
+            "descricao": "Cursos gratuitos de programação e tecnologia para estudantes do ensino médio.",
+            "local": "Belo Horizonte",
+            "pessoas_atendidas": 1600,
+            "voluntarios": 30,
+            "imagens": [
+                {
+                    "id": 1,
+                    "link": "https://images.unsplash.com/photo-1515879218367-8466d910aaa4"
+                },
+                {
+                    "id": 2,
+                    "link": "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbWElQzMlQTclQzMlQTNvfGVufDB8fDB8fHwy"
+                },
+                {
+                    "id": 3,
+                    "link": "https://images.unsplash.com/photo-1550439062-609e1531270e?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2dyYW1hJUMzJUE3JUMzJUEzb3xlbnwwfHwwfHx8Mg%3D%3D"
+                },
+                {
+                    "id": 4,
+                    "link": "https://images.unsplash.com/photo-1629904853716-f0bc54eea481?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHByb2dyYW1hJUMzJUE3JUMzJUEzb3xlbnwwfHwwfHx8Mg%3D%3D"
+                }
+            ],
+            "redes_sociais": [
+                {
+                    "id": 1,
+                    "nome": "WhatsApp",
+                    "link": "wa.me/553199999999"
+                }
+            ]
+        }
+    ]
+};
+
+function carregarCarrossel() {
+    const indicadores = document.getElementById("carouselIndicators");
+    const carouselInner = document.getElementById("carouselInner");
+
+    indicadores.innerHTML = "";
+    carouselInner.innerHTML = "";
+
+    dados.projetos.slice(0,3).forEach((projeto, index) => {
+
+        // Indicadores
+        indicadores.innerHTML += `
+            <button
+                type="button"
+                data-bs-target="#myCarousel"
+                data-bs-slide-to="${index}"
+                class="${index === 0 ? "active" : ""}"
+                ${index === 0 ? 'aria-current="true"' : ""}
+                aria-label="Slide ${index + 1}">
+            </button>
+        `;
+
+        // Slides
+        carouselInner.innerHTML += `
+            <div class="carousel-item ${index === 0 ? "active" : ""}">
+                <img
+                    src="${projeto.imagens[0].link}"
+                    class="d-block w-100 carousel-img"
+                    alt="${projeto.nome}"
+                    style="height:500px; object-fit:cover;"
+                >
+
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>${projeto.nome}</h1>
+                        <p>${projeto.descricao}</p>
+
+                        <p>
+                            <a href="detalhes.html?id=${projeto.id}" class="btn">Detalhes</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+
+if (document.getElementById("carouselIndicators")) {
+    carregarCarrossel();
+}
+
+function carregarCards() {
+    const container = document.getElementById("listaProjetos");
+
+    dados.projetos.forEach((projeto) => {
+        container.innerHTML += `
+            <div class="col">
+                <div class="card h-100 shadow-sm">
+                    <img
+                        src="${projeto.imagens[0].link}"
+                        class="card-img-top card-img-projeto"
+                        alt="${projeto.nome}"
+                    >
+
+                    <div class="card-body">
+                        <h5 class="card-title">${projeto.nome}</h5>
+                        <p class="card-text">${projeto.descricao}</p>
+
+                        <a href="detalhes.html?id=${projeto.id}" class="btn">Detalhes</a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+
+if (document.getElementById("listaProjetos")) {
+    carregarCards();
+}
+
+
+function carregarGaleria(projeto) {
+
+    const galeria = document.getElementById("galeria-projeto");
+
+    galeria.innerHTML = "";
+
+    projeto.imagens.forEach((imagem) => {
+
+        galeria.innerHTML += `
+            <div class="col-md-4 col-sm-6">
+                <img
+                    src="${imagem.link}"
+                    alt="${projeto.nome}"
+                    class="img-fluid rounded shadow foto-galeria"
+                    style="width:200px; height:200px; object-fit:cover;"
+                >
+            </div>
+        `;
+
+    });
+}
+
+function exibirDetalhes() {
+    const parametros = new URLSearchParams(window.location.search);
+
+    const id = parametros.get("id");
+
+    const projeto = dados.projetos.find(p => p.id == id);
+
+    if(!projeto){
+        return;
+    }
+
+    document.getElementById("foto-projeto").src = `${projeto.imagens[0].link}`;
+
+    document.getElementById("nome-projeto").innerText = projeto.nome;
+
+    document.getElementById("detalhes-projeto").innerText = projeto.descricao;
+
+    document.getElementById("localizacao-projeto").innerHTML = `<i class="bi bi-geo-alt-fill"></i> ${projeto.local}`;
+
+    document.getElementById("pessoas-atendidas").innerHTML = `Pessoas atendidas: ${projeto.pessoas_atendidas}`;
+
+    document.getElementById("voluntarios").innerHTML = `Voluntários: ${projeto.voluntarios}`;
+
+    carregarGaleria(projeto);
+
+}
+
+if (document.getElementById("informacoesProjeto")) {
+    exibirDetalhes();
+}
+
